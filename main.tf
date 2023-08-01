9a3510e22b00549e9c855f7d11e5b6ba7e11e862
MungaiVic <victornmungai@gmail.com>
2023-08-01 11:17:28 +0300

    Update terraform file

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "local" {}
 
 resource "local_file" "ssh_config" {
   filename = "${path.module}/ssh_config"
-  content = <<-EOT
+  content  = <<-EOT
     Host vagrant
       HostName 127.0.0.1
       Port 2222
@@ -15,14 +15,7 @@ resource "local_file" "ssh_config" {
 
 resource "null_resource" "vagrant" {
   provisioner "local-exec" {
-    command     = "vagrant init geerlingguy/ubuntu2004 && vagrant up"
-    working_dir = path.module
-  }
-}
-
-resource "null_resource" "ansible_provisioner" {
-  provisioner "local-exec" {
-    command     = "ansible-playbook -i ../hosts ../playbook.yml --private-key .vagrant/machines/default/virtualbox/private_key -u vagrant"
+    command     = "vagrant up --provision"
     working_dir = path.module
   }
 }
